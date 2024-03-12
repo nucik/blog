d20a06be2c520ab23a69a5b1a098dc3ca4b687e7
Nuciko Dekanzishvili <163172607+nucik@users.noreply.github.com>
2024-03-12 22:32:00 +0400

    Initial commit

---
+++ _pages/about.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: About
+# permalink: /about/
+---
+
+A page about you and a link [example.com](https://example.com/)
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_pages/*.md",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: [],
+};
